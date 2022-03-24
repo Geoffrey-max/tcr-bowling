d461e7242eea92b615baa5d9d889ac026193a31c
Geoffrey_max <geoffrey.clermont@ynov.com>
2022-03-24 09:46:08 +0100

    Working version

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class BowlingGame
+{
+    private int score;
+    private int maxThrows;
+    private int iterations;
+
+    public BowlingGame(int score)
+    {
+        this.score = score;
+    }
+
+    public void playGame()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            
+        }
+    }
+}
