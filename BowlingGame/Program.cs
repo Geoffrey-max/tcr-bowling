f8311b0a684aac96aeac052485cb1cf71e22b2e8
Geoffrey_max <geoffrey.clermont@ynov.com>
2022-03-24 09:59:21 +0100

    fix class

--- BowlingGame/Program.cs
+++ BowlingGame/Program.cs
@@ -1,22 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-public class BowlingGame
+namespace BowlingGame
 {
-    private int score;
-    private int maxThrows;
-    private int iterations;
-
-    public BowlingGame(int score)
+    public class BowlingGame
     {
-        this.score = score;
-    }
+        private int score;
+        private int maxThrows;
+        private int iterations;
 
-    public void playGame()
-    {
-        for (int i = 0; i < 10; i++)
+        public BowlingGame(int score)
         {
+            this.score = score;
+        }
+
+        public void playGame()
+        {
+            for (int i = 0; i < 10; i++)
+            {
             
+            }
         }
     }
 }
+
