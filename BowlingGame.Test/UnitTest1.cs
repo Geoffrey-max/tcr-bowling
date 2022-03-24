7c0432fa1fc2cfb48a5f66e76a3dd2ea34850f07
Adrien VRD <adrien.vaucard@gmail.com>
2022-03-24 11:27:15 +0100

    update test

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -26,15 +26,36 @@ namespace BowlingGame.Test
             //nbPlayers : list of Players
             var game = new BowlingGame(players);
 
-            foreach (var player in players)
-            {
-                Assert.IsNotNull(player.launchThrow());
-                Assert.IsTrue(player.score >= 0);
-            }
-            
             // Assert
             Assert.AreEqual(players.Length, game.players.Length);
             
+        }
+        
+        [TestMethod]
+        public void TestPlayerThrow()
+        {
+            // Arrange
+            int maxThrows = 2;
+            Player player1 = new Player("Maya Hi");
+            Player player2 = new Player("Maya Hou");
+            Player player3 = new Player("Maya Ha");
+            Player player4 = new Player("Maya Ha ha");
+            Player[] players = new Player[3];
+            players[0] = player1;
+            players[1] = player2;
+            players[2] = player3;
+            players[3] = player4;
+
+            
+            //Constructor BowlingGame(score, nbPlayers)
+            //nbPlayers : list of Players
+            var game = new BowlingGame(players);
+
+            foreach (var player in players)
+            {
+                Assert.IsNotNull(player.launchThrow());
+                Assert.IsTrue(player.launchThrow() >= 0 && player.launchThrow() <= 10);
+            }
         }
     }
 }
